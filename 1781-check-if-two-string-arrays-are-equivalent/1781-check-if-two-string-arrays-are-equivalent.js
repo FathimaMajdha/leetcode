364ae9e19f6842766985748ffbbafd62e0d69291
Fathima Majdha <102402807+FathimaMajdha@users.noreply.github.com>
2025-02-20 10:04:25 +0530

    Time: 0 ms (100%), Space: 54.2 MB (5.21%) - LeetHub

--- 1781-check-if-two-string-arrays-are-equivalent/1781-check-if-two-string-arrays-are-equivalent.js
+++ 1781-check-if-two-string-arrays-are-equivalent/1781-check-if-two-string-arrays-are-equivalent.js
@@ -4,18 +4,16 @@
  * @return {boolean}
  */
 var arrayStringsAreEqual = function (word1, word2) {
-    let first = "";
-    let second = ""
+
     for (let i = 0; i < word1.length; i++) {
         for (let i = 0; i < word2.length; i++) {
-            first += word1.join('')
-            second += word2.join('')
+            if (word1.join('') == word2.join('')) {
+                return true
+            } else {
+                return false
+            }
+
         }
-    }
-    if (first == second) {
-        return true
-    } else {
-        return false
     }
 
 };
