d6b39c952ad009cfabe19b948d32545c1ca497ad
Fathima Majdha <102402807+FathimaMajdha@users.noreply.github.com>
2025-02-28 09:46:43 +0530

    Time: 0 ms (100%), Space: 38.7 MB (58.78%) - LeetHub

---
+++ 1737-maximum-nesting-depth-of-the-parentheses/1737-maximum-nesting-depth-of-the-parentheses.cs
@@ -0,0 +1,15 @@
+public class Solution {
+    public int MaxDepth(string s) {
+        int maxDepth = 0, currentDepth=0;
+
+        foreach(char c in s){
+            if(c == '('){
+                currentDepth++;
+                maxDepth = Math.Max(maxDepth,currentDepth);
+            }else if(c==')'){
+                currentDepth--;
+            }
+        }
+        return maxDepth;
+    }
+}
