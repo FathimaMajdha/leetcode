a89e5a5fd36e12ee717e9e8c979469174ec88f83
Fathima Majdha <102402807+FathimaMajdha@users.noreply.github.com>
2025-03-28 05:17:53 +0530

    Time: 845 ms (11.63%), Space: 57 MB (6.98%) - LeetHub

---
+++ 0010-regular-expression-matching/0010-regular-expression-matching.cs
@@ -0,0 +1,13 @@
+public class Solution {
+    public bool IsMatch(string s, string p) {
+        if (p.Length == 0) return s.Length == 0;
+
+        bool firstMatch = (s.Length > 0 && (s[0] == p[0] || p[0] == '.'));
+
+        if (p.Length > 1 && p[1] == '*') {
+            return IsMatch(s, p.Substring(2)) || (firstMatch && IsMatch(s.Substring(1), p));
+        } else {
+            return firstMatch && IsMatch(s.Substring(1), p.Substring(1));
+        }
+    }
+}
