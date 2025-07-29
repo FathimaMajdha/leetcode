09361d69e6cadb58340b3ae5173a82886fae2f94
Fathima Majdha <102402807+FathimaMajdha@users.noreply.github.com>
2025-07-29 22:51:35 +0530

    Time: 0 ms (100%), Space: 43.3 MB (82.29%) - LeetHub

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.cs
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.cs
@@ -9,14 +9,7 @@
  *     }
  * }
  */
-// public class ListNode {
-//     public int val;
-//     public ListNode next;
-//     public ListNode(int val = 0, ListNode next = null) {
-//         this.val = val;
-//         this.next = next;
-//     }
-// }
+
 
 public class Solution {
     public ListNode MergeTwoLists(ListNode list1, ListNode list2) {
